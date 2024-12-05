4123d86dbec31f83147c15f1cd7536504d98bfb8
eezadenALsalahy <eezzaaddeenn778918@gmail.com>
2024-12-05 23:55:26 +0300

    Add files via upload

---
+++ hessaap/hessaap/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hessaap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //if (textBox1.Text.Trim() == "")
+            //{
+            // MessageBox.Show(";)"ادخل العدد الاول
+            // textBox1.Focus();
+            // return;
+            //}
+            //else if (textBox2.Text.Trim() == "")
+            //{
+            // MessageBox.Show
+            // textBox2.Focus();
+            //}
+            //else
+            //{
+            //double x = Convert.ToDouble(textBox1.Text);
+            //double y = Convert.ToDouble(textBox2.Text);
+            //double z = x + y;
+            //textBox3.Text = z.ToString();
+            //textBox1.Text = textBox2.Text ="";
+            //textBox1.Text = textBox2.Text = null;+
+            //textBox1.Clear();+
+            //textBox2.Clear();+
+            //textBox3.Text= textBox1.Text+ textBox2.Text;
+
+           
+        }
+    }
+}
